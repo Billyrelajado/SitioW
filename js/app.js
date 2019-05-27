8d5dc752d3569a6431407b9af656c333678534dc
billyrelajado <billy.e4e5@gmail.com>
2019-05-26 21:27:43 -0500

    adding url in js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 function sent_dt(){
     var the_host = window.location.origin;
     alert(the_host);
-    var newurl = the_host+"drunk.html"
+    var newurl = the_host+"/SitioW/drunk.html"
     alert(newurl);
     window.location.href = newurl;//"./drunk.html";
 }
